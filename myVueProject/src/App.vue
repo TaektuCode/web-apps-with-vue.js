72e4be8f4ed697cc85e5c430c20c4a0e611918f7
taektu_coding <taektu.coding@gmail.com>
2023-11-15 11:33:23 +0100

    feat: adds header and booklist component

---
+++ myVueProject/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LayoutHeader from "./components/LayoutHeader.vue";
+import BooList from "./components/BooList.vue";
+</script>
+
+<template>
+  <LayoutHeader />
+  <main>
+    <div id="app" class="inside">
+      <BooList />
+    </div>
+  </main>
+</template>
